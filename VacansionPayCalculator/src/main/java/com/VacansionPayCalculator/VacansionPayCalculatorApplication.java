a3fa702764e68953f45e92e4d5ad88b3c6416295
Kholodkov <holodnsk@gmail.com>
2023-05-04 20:20:39 +0700

    init: boot starter application running

---
+++ VacansionPayCalculator/src/main/java/com/VacansionPayCalculator/VacansionPayCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.VacansionPayCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacansionPayCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacansionPayCalculatorApplication.class, args);
+	}
+
+}
---
+++ VacansionPayCalculator/src/test/java/com/VacansionPayCalculator/VacansionPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VacansionPayCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacansionPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
